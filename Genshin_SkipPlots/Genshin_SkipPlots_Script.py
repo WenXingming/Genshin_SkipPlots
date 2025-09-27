ec0f23f6517af87ccdfdc5ca9f6df8a4dcb61091
wxm <2801650811@qq.com>
2025-09-27 15:32:09 +0800

    增加自动行走，主要是为了剧情中跟踪认为行走

--- Genshin_SkipPlots/Genshin_SkipPlots_Script.py
+++ Genshin_SkipPlots/Genshin_SkipPlots_Script.py
@@ -3,7 +3,7 @@
     功能：通过自动点击鼠标跳过游戏剧情
     使用方法：（如出现失效请）以管理员身份运行本程序
     注意事项：请确保游戏窗口处于活动状态，且点击位置正确设置
-    控制键：F12 - 退出程序，F9 - 暂停/继续
+    控制键：F12 - 退出程序，F11 - 暂停/继续，F10 - 开启/关闭剧情按键，F9 - 开启/关闭行走按键
 
     依赖库：keyboard, pyautogui
     安装依赖：pip install keyboard pyautogui
@@ -16,7 +16,8 @@ import time
 
 
 class GameSkipScript:
-    def __init__(self, click_position=(1500, 740), click_interval=1):
+    # 添加剧情按键和行走按键
+    def __init__(self, click_position=(1500, 740), click_interval=1,  storyline_key="f", movement_key="w"):
         """
         初始化脚本
 
@@ -24,9 +25,16 @@ class GameSkipScript:
             click_position: 点击坐标 (x, y)
             click_interval: 点击间隔时间(秒)
         """
-        self.is_running = True
+        self.should_exit = False  # 退出标志，用于退出程序
+        self.is_running = True     # 运行状态，用于暂停/继续
+
         self.click_position = click_position
         self.click_interval = click_interval
+
+        self.storyline_key = storyline_key
+        self.start_storyline_key = False
+        self.movement_key = movement_key
+        self.start_movement_key = False
 
         # 获取屏幕分辨率
         self.screen_width, self.screen_height = pyautogui.size()
@@ -38,30 +46,39 @@ class GameSkipScript:
     def _setup_keyboard_listener(self):
         """设置键盘事件监听器"""
         keyboard.on_press(self._keyboard_callback)
-        print("键盘监听器已设置 - F12:退出, F9:暂停/继续")
+        print("键盘监听器已设置 - F12: 退出, F11: 暂停/继续, F10: 开启/关闭剧情按键, F9: 开启/关闭行走按键")
 
     def _keyboard_callback(self, event):
         """键盘事件回调函数"""
         if event.name == "f12":  # 退出程序
-            print("收到退出指令，程序即将退出...")
             keyboard.unhook_all()
-
-            # 设置退出标志，让主线程结束
             self.should_exit = True
+
+            print("收到退出指令，程序即将退出...")
+            pyautogui.alert("程序即将退出", "提示")  # 弹出提示框，放在设置退出标志后面
             # 按下 f12 程序无法退出：问题在于 keyboard.wait() 会阻塞主线程，导致 exit() 无法立即生效。 需要修改退出机制。
             # exit()
-
-            # 弹出提示框，放在设置退出标志后面
-            pyautogui.alert("程序即将退出", "提示")
-        elif event.name == "f9":  # 暂停/继续
+        elif event.name == "f11":  # 暂停/继续
             self.is_running = not self.is_running
+
             status = "继续运行" if self.is_running else "暂停"
             print(f"程序已{status}")
+            if not self.is_running:  # 弹出提示框，放在状态改变后面
+                pyautogui.alert("程序已暂停，请先关闭此弹窗。按 F9 可继续", "提示")  # 暂停时弹出提示框，继续时不弹出
+        elif event.name == "f10":
+            self.start_storyline_key = not self.start_storyline_key
 
-            # 弹出提示框，放在状态改变后面
-            if(not self.is_running):
-                pyautogui.alert("程序已暂停，请先关闭此弹窗。按 F9 可继续", "提示") # 暂停时弹出提示框，继续时不弹出
-            
+            status = "开启" if self.start_storyline_key else "关闭"
+            print(f"剧情按键已{status}")
+            if not self.start_storyline_key:
+                pyautogui.alert(f"剧情按键已{status}", "提示")
+        elif event.name == "f9":
+            self.start_movement_key = not self.start_movement_key
+
+            status = "开启" if self.start_movement_key else "关闭"
+            print(f"行走按键已{status}")
+            if not self.start_movement_key:
+                pyautogui.alert(f"行走按键已{status}", "提示")
 
     def _calculate_relative_position(self):
         """计算相对坐标位置"""
@@ -85,9 +102,12 @@ class GameSkipScript:
         while True:
             if self.is_running:
                 # pyautogui.click(x=absolute_x, y=absolute_y, button="left")
-                # 改为按 下 'f' 键
-                pyautogui.press("f")
+                if self.start_storyline_key:
+                    keyboard.press_and_release(self.storyline_key)  # 改为按 下 'f' 键
+                if self.start_movement_key:
+                    keyboard.press(self.movement_key)  # 持续按 'w' 键
             time.sleep(self.click_interval)
+            keyboard.release(self.movement_key)  # 松开 'w' 键
 
     def _show_warning(self):
         """显示警告信息"""
@@ -98,7 +118,9 @@ class GameSkipScript:
             "3. 点击位置正确设置\n\n"
             "控制键说明：\n"
             "F12 - 退出程序\n"
-            "F9 - 暂停/继续"
+            "F11 - 暂停/继续\n"
+            "F10 - 开启/关闭剧情按键\n"
+            "F9  - 开启/关闭行走按键\n"
         )
         pyautogui.alert(warning_msg, "原神跳过脚本")
 
@@ -132,6 +154,8 @@ class GameSkipScript:
 
             # 保持主线程运行。按下 f12 程序无法退出：问题在于 keyboard.wait() 会阻塞主线程，导致 exit() 无法立即生效。需要修改退出机制。
             # keyboard.wait()
+            # keyboard.wait("f12")  # 等待按下 f12 键
+            # time.sleep(3)  # 等待 3 秒以确保点击线程结束
             while not self.should_exit:
                 time.sleep(10)  # 主线程每 10 秒检查一次退出标志
             print("程序退出完成")
@@ -158,12 +182,14 @@ def main():
     game_type = "zzz"  # 修改这里切换游戏
 
     # 创建并启动脚本
+    click_interval = 1  # 点击间隔时间(秒)
+    countdown_seconds = 3  # 倒计时秒数
     script = GameSkipScript(
         click_position=game_positions.get(game_type, game_positions["default"]),
-        click_interval=1,  # 点击间隔1秒
+        click_interval=click_interval,
     )
 
-    script.start(countdown_seconds=5)
+    script.start(countdown_seconds=countdown_seconds)
 
 
 if __name__ == "__main__":
